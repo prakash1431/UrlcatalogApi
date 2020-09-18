659a5f91f26abf295dbb259d8cc48efdc8d75575
Surya Prakash <prakash1431@gmail.com>
2020-09-18 05:40:04 +0530

    unit tests and heroku deployment test

---
+++ UrlcatalogApiTest/AccountControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using DataService.Interfaces;
+using ModelService;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiCatalog.Controllers.Api.v1;
+
+namespace UrlBookApiTest
+{
+    public class AccountControllerTest
+    {
+        AccountController _controller;
+        private readonly Mock<IUserSvc> _userServiceMock = new Mock<IUserSvc>();
+        private readonly Mock<IAuthSvc> _authServiceMock = new Mock<IAuthSvc>();
+        public AccountControllerTest()
+        {
+            _controller = new AccountController(_userServiceMock.Object, _authServiceMock.Object);
+        }
+
+        [Test]
+        public void Test_Register()
+        {
+            var model = new RegisterViewModel()
+            {
+                Email ="purukumar16@gmail.com",
+                Firstname ="purushottam",
+                Lastname ="kumar",
+                Password="12345"
+
+            };
+
+            var response = new ResponseObject()
+            {
+                IsValid = true,
+                Message = "Success",
+                Data = new { username = "Test123", email = "Test@gmail.com", status = 1, message = "Registration Successful" }
+            };
+
+            _userServiceMock.Setup(x => x.RegisterUserAsync(model)).ReturnsAsync(response);
+
+            var result = _controller.Register(model);
+            Assert.IsTrue(result.IsCompleted);
+        }
+    }
+}
---
+++ UrlcatalogApiTest/ProfileControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using DataService.Interfaces;
+using ModelService;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using WebApiCatalog.Controllers.Api.v1;
+
+namespace UrlBookApiTest
+{
+    public class ProfileControllerTest
+    {
+        private readonly ProfileController _controller;
+        private readonly Mock<IUserSvc> _userServiceMock = new Mock<IUserSvc>();
+
+        public ProfileControllerTest()
+        {
+            _controller = new ProfileController(_userServiceMock.Object);
+        }
+
+        [Test]
+        public void Get_All_Users_Test()
+        {
+            var users = _controller.GetAllUsers();
+            Assert.IsNotNull(users);
+        }
+
+        [Test]
+        public void UpdateProfile_Test()
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                Firstname ="Purushottam",
+                Lastname ="Kumar",
+                UserRole ="Admin",
+                isAdmin = true
+            };
+
+            _userServiceMock.Setup(x => x.UpdateProfileAsync(user));
+
+            var userList = new List<ApplicationUser>() { user};
+
+            var users = _controller.UpdateProfile(userList);
+            Assert.That(users.IsCompletedSuccessfully);
+        }
+    }
+}
